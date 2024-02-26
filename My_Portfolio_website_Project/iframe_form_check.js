9d64641bfde199a3d85bbf017f54c549b2f1de72
Yashwant Kumar Sharma <161282944+Yashwantkumar528@users.noreply.github.com>
2024-02-26 12:13:35 +0530

    Add files via upload
    I developed this portfolio project using HTML , CSS , JS , BOOSTRAP.

---
+++ My_Portfolio_website_Project/iframe_form_check.js
@@ -0,0 +1,24 @@
+/*!
+ * 
+ *     MCAFEE RESTRICTED CONFIDENTIAL
+ *     Copyright (c) 2024 McAfee, LLC
+ *
+ *     The source code contained or described herein and all documents related
+ *     to the source code ("Material") are owned by McAfee or its
+ *     suppliers or licensors. Title to the Material remains with McAfee
+ *     or its suppliers and licensors. The Material contains trade
+ *     secrets and proprietary and confidential information of McAfee or its
+ *     suppliers and licensors. The Material is protected by worldwide copyright
+ *     and trade secret laws and treaty provisions. No part of the Material may
+ *     be used, copied, reproduced, modified, published, uploaded, posted,
+ *     transmitted, distributed, or disclosed in any way without McAfee's prior
+ *     express written permission.
+ *
+ *     No license under any patent, copyright, trade secret or other intellectual
+ *     property right is granted to or conferred upon you by disclosure or
+ *     delivery of the Materials, either expressly, by implication, inducement,
+ *     estoppel or otherwise. Any license under such intellectual property rights
+ *     must be expressed and approved by McAfee in writing.
+ *
+ */(()=>{"use strict";const e=0,t=0,o=1,r=2,s=3,n=4,a=1,i=2,c=3,l=4,d={BACKGROUND:"BACKGROUND",CONTENT:"CONTENT",TELEMETRY:"TELEMETRY"},u={DEFAULT:"color: #000000; font-weight: normal; font-style:normal; background: #FFFFFF;",BACKGROUND:"color: #8D0DBA; font-weight: bold; background: #FFFFFF;",CONTENT:"color: #F54A26; font-weight: bold; background: #FFFFFF;",TELEMETRY:"color: #147831; font-weight: bold; background: #FFFFFF;"};class m{static log(e,t=null){h(e,a,t)}static error(e,t=null){h(e,i,t)}static warn(e,t=null){h(e,c,t)}static debug(e,t=null){h(e,l,t)}}const h=(m,h,F)=>{const f=e;if(f===t)return;let g="chrome-extension:"===location.protocol?d.BACKGROUND:d.CONTENT;F&&d[F]&&(g=F);const w=new Date,b=h===i?m:`%c[${g} ${w.toLocaleString([],{hour:"2-digit",minute:"2-digit",hour12:!0})}]: %c${m}`,p=u.DEFAULT;let C=u[g];if(C||(C=p),f>=r&&h===i&&console.error(m),f>=o&&h===a&&console.log(b,C,p),f>=s&&h===c){const e="color: #FFA500; font-family: sans-serif; font-weight: bolder; text-shadow: #000 1px 1px;";console.log(`%cWARN - ${b}`,e,C,p)}if(f>=n&&h===l){const e="color: #FF33D7; font-family: sans-serif; font-weight: bolder; text-shadow: #000 1px 1px;";console.log(`%cDEBUG - ${b}`,e,C,p)}},F=async(e,t,o,r)=>{try{return await f(e,t,o,r),!0}catch(e){return m.warn(`[broadcast] Unexpected error when calling command: "${t}", err: ${e.message}`),null}},f=(e,t,o,r,s=null)=>{if(!chrome.tabs)throw new Error('"tabs" permission not set in manifest.');return chrome.tabs.sendMessage(r,{ipcId:e,command:t,...o},{frameId:s})},g=(e,t={},o)=>(async(e,t,o={},r={})=>{try{if(r?.tabId){const{tabId:s,frameId:n}=r;return await f(e,t,o,s,n)}if(r?.broadcast){const s=await chrome.tabs.query({}),{broadcastIgnoreId:n=[]}=r;return s.filter((({id:e})=>!n.includes(e))).forEach((({id:r})=>{F(e,t,o,r)})),!0}return await chrome.runtime.sendMessage({ipcId:e,command:t,...o})}catch(e){return m.warn(`Unexpected error when calling command: "${t}", err: ${e.message}`),null}})("WA",e,t,o),w="GET_TAB_AND_FRAME_ID",b="FORM_PRE_CHECK_PASSED";(new class{constructor(){this.pingCommand="PING_IFRAME_FORM_CHECK",this.basePingListener()}basePingListener(e=null){((e,t=null,o)=>{"function"==typeof o?chrome.runtime.onMessage.addListener(((r,s,n)=>{if(s.id===chrome.runtime.id&&"object"==typeof r&&!Array.isArray(r)&&r?.ipcId===e)return o({promises:t,request:r,sender:s,sendResponse:n})})):m.error("Provided with invalid callback function")})("WA",null,(({request:t,sendResponse:o})=>{const{command:r}=t;if(r===this.pingCommand)return o({content:!0}),"function"==typeof e&&e(),!0}))}isPossibleFormPage(){let e=[...document.getElementsByTagName("input")];return e=e.filter((e=>!(e.name.toLowerCase().includes("search")||(e.ariaLabel?e.ariaLabel.toLowerCase():"").includes("search")||e.id.toLowerCase().includes("search")||e.className.toLowerCase().includes("search")||e.defaultValue.toLowerCase().includes("search")||e.value.toLowerCase().includes("search")||"hidden"===e.type.toLowerCase()||"checkbox"===e.type.toLowerCase()||"submit"===e.type.toLowerCase()||"search"===e.type.toLowerCase()||"file"===e.type.toLowerCase()||"button"===e.type.toLowerCase()))),e.length>0}async main(){const{frameId:e}=await g(w);if(this.isPossibleFormPage())g(b);else if(0===e){const e=new MutationObserver((()=>{this.isPossibleFormPage()&&(g(b),e.disconnect())})),t={childList:!0,subtree:!0};e.observe(document,t)}else setTimeout((()=>{this.isPossibleFormPage()&&g(b)}),1e3)}}).main()})();
+//# sourceMappingURL=../sourceMap/chrome/scripts/iframe_form_check.js.map
